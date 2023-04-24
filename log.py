4b42c8848cc1e437b2330cc72ba21fa99fb2e7b0
tarcisiolopes <tarcisiolopesgomes@gmail.com>
2023-04-24 15:32:07 -0300

    Log

---
+++ log.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+def write_log(logger_name, level=logging.DEBUG):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    format_string = ("%(asctime)s :: lvl: %(levelname)s :: line: %(lineno)d :: funcName: %(funcName)s :: %(message)s")
+    log_format = logging.Formatter(format_string)
+    
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(log_format)
+    logger.addHandler(console_handler)
+    
+    file_handler = logging.FileHandler("LOGS/"+logger_name+".log", mode='a')
+    file_handler.setFormatter(log_format)
+    logger.addHandler(file_handler)
+    return logger
--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import pandas as pd
-import datetime
-import psycopg2 as pg
-import sys
-import logging
+
 
